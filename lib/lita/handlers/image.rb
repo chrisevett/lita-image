70c749606d35a2bfc3037d453a8479285a8bf741
Evett, Christopher <cevett@cardlytics.com>
2016-05-24 16:39:15 -0400

    remove the bad applause nobody ever wants to see

--- lib/lita/handlers/image.rb
+++ lib/lita/handlers/image.rb
@@ -5,7 +5,6 @@ module Lita
       ApplauseImages = [
         'http://i.imgur.com/t8zvc.gif',
         'http://i.imgur.com/5zKXz.gif',
-        'http://www.animateit.net/data/media/july2012/clap-animated-animation-clap-000340-large.gif',
         'http://i.imgur.com/9Zv4V.gif',
         'http://assets0.ordienetworks.com/images/GifGuide/clapping/1292223254212-dumpfm-mario-Obamaclap.gif',
         'http://www.reactiongifs.com/wp-content/uploads/2013/01/applause.gif',
